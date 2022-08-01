22cf4ffd3ddc2fb0dc75e7a4c6f893798c6c3129
AmitJ111 <110393157+AmitJ111@users.noreply.github.com>
2022-08-01 18:29:30 +0530

    Update GetHubDemo.java
    Edited by amit.

--- GitHub1/src/amit/GetHubDemo.java
+++ GitHub1/src/amit/GetHubDemo.java
@@ -2,8 +2,15 @@ package amit;
 
 public class GetHubDemo {
 
-
+public void m1()
+{
+	System.out.println("welcome to GitHub");
+	
 	public static void main(String args[]) {
 		System.out.println("GitHub Start");
+		GetHubDemo a= new GetHubDemo();
+		a.m1();
+		System.out.println("end");
+		
 	}
 }
